5b468aedeadaadb3b6bf99d5a7db0e437eba93d0
ergun1017 <erdogmusergun@gmail.com>
2018-03-01 17:38:58 +0300

    Updated dev-server output config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,9 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
+    output: {
+        filename: "[name].js"
+    },
     devServer: {
         port: 8080,
         publicPath: "/",
